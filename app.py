1e7df406f54198fb0c75b56a99c67a7d7c571551
Andrew <al4991@nyu.edu>
2018-11-14 15:55:18 -0500

    Modifying login and general structure of app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, session, redirect
-from flask import url_for, escape, request 
+from flask import url_for, escape, request, flash 
 import MySQLdb
 
 app = Flask(__name__)
@@ -8,29 +8,49 @@ app.secret_key = "Doesn'tMatterRn"
 
 db = MySQL.db.connect(host="localhost", user="root", 
 	                  passwd = "", db="Pricosha")
+place = db.cursor()
 
 
 @app.route("/")
 def index():
-    if 'username' in session:
-    	return 'Logged in as %s' % escape(session['username'])
+    if 'useremail' in session:
+    	return 'Logged in as %s' % escape(session['useremail'])
+
     return "You ain't logged in"
+
 
 @app.route('/login', methods=['GET','POST'])
 def login():
-	if request.method == 'POST':
-		session['username'] = request.form['username']
+	if 'useremail' in session: 
 		return redirect(url_for('index'))
-	return '''
-	    <form method="post"> 
-	        <p><input type=text name=username>
-	        <p><input type=submit value=Login>
-	    </form>
-	       '''
+
+	if request.method == 'POST':
+		user_email = request.form['email']
+	    password = request.form['password']
+	    place.execute("SELECT count(*) FROM Person WHERE email = %s;",[user_email])
+	    if place.fetchone()[0]:
+	    	place.execute("SELECT password FROM Person WHERE email = %s;", [user_email])
+	    	row = place.fetchone(): 
+	    		if password == row[0]:
+	    			session['useremail'] = request.form['useremail']
+	    			return redirect(url_for('index'))
+	    		else:
+	    			flash('Something is wrong')
+
+
+	    else:
+	    	flash("Something is wrong pops")
+	   	return render_template('login.html')
+
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup(): 
+	return ''
 
 @app.route('/logout')
 def logout(): 
-	session.pop('username', None)
+	session.pop('useremail', None)
 	return redirect(url_for('index'))
 
 
