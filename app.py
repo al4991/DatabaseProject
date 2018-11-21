0abdf4d56e27a75eb87fa0b153279f43825efc99
Andrew <al4991@nyu.edu>
2018-11-21 11:57:37 -0500

    Cleaned up code in app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, session, redirect
-from flask import url_for, escape, request, flash 
+from flask import url_for, request
 import pymysql.cursors
 
 app = Flask(__name__)
@@ -7,11 +7,11 @@ app.secret_key = "Doesn'tMatterRn"
 
 # Setting up MySQL
 conn = pymysql.connect(host='localhost',
-	                 user='root',
-	                 password='root',
-	                 db='pricosha',
-	                 charset='utf8mb4',
-	                 cursorclass=pymysql.cursors.DictCursor)
+                       user='root',
+                       password='',
+                       db='pricosha',
+                       charset='utf8mb4',
+                       cursorclass=pymysql.cursors.DictCursor)
 
 
 @app.route("/")
@@ -27,6 +27,7 @@ def index():
 @app.route('/login')
 def login(): 
     return render_template('login.html')
+
 
 @app.route('/loginAuth', methods=['GET', 'POST'])
 def loginAuth():
@@ -45,7 +46,7 @@ def loginAuth():
     cursor.close()
 
     # Checking to see if the login info actually exists or not 
-    if (data):
+    if data:
         # creates a session for the user
         session['userEmail'] = user_email
         # redirecting user to our main page
@@ -59,30 +60,30 @@ def loginAuth():
     else: 
         # Means we didn't find the login info, so failed login
         # We create an error to pass to our html
-        #flash("Something is wrong. Login failed")
-        #error = "Invalid email or password"
-        #return render_template('login.html', error=error)
+        # error = "Invalid email or password"
+        # return render_template('login.html', error=error)
         error = "Invalid email or password"
         return render_template('login.html', error=error)
 
+
 @app.route('/register')
-def register(): 
-	return render_template('signup.html')
+def register():
+    return render_template('signup.html')
+
 
 @app.route('/registerAuth', methods=['GET', 'POST'])
 def registerAuth():
-    #grabs information from the forms
+    # grabs information from the forms
     user_email = request.form['userEmail']
     password = request.form['password']
-
-    #cursor used to send queries
+    # cursor used to send queries
     cursor = conn.cursor()
-    #executes query
+    # executes query
     query = 'SELECT * FROM PERSON WHERE email = %s'
-    cursor.execute(query, (user_email))
+    cursor.execute(query, user_email)
 
     data = cursor.fetchone()
-    if(data):
+    if data:
         error = "This user already exists"
         cursor.close()
         return render_template('signup.html', error=error)
@@ -94,7 +95,8 @@ def registerAuth():
         cursor.close()
         return render_template('index.html')
 
-@app.route('/post',methods=['GET','POST'])
+
+@app.route('/post', methods=['GET', 'POST'])
 def post():
     user_email = session['userEmail']
     cursor = conn.cursor()
@@ -104,6 +106,7 @@ def post():
     conn.commit()
     cursor.close()
     return redirect(url_for('home'))
+
 
 @app.route('/home')
 def home():
@@ -121,5 +124,6 @@ def logout():
     session.pop('userEmail')
     return redirect('/')
 
+
 if __name__ == "__main__":
-    app.run('127.0.0.1', 5000, debug = True)
+    app.run('127.0.0.1', 5000, debug=True)
