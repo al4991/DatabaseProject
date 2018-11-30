ed88b32e88c91a42117b6af66be6ba4a6cd38300
carolynmccawley <cam1010@nyu.edu>
2018-11-29 19:06:33 -0500

    adding ability to create group and add friends

--- app.py
+++ app.py
@@ -127,6 +127,90 @@ def logout():
     session.pop('userEmail')
     return redirect('/')
 
+@app.route('/newGroup')
+def newGroup():
+    displayNewGroup = "true"
+    return render_template('newGroup.html', displayNewGroup = displayNewGroup)
+
+@app.route('/createNewGroup', methods=['GET','POST'])
+def createNewGroup():
+    user_email = session['userEmail']
+    groupName = request.form['groupName']
+    groupDesc = request.form['groupDesc']
+
+    # Check if group already exists!!!!
+    cursor = conn.cursor()
+    checkQuery = 'SELECT fg_name FROM FriendGroup WHERE owner_email = (%s) AND fg_name = (%s)'
+    cursor.execute(checkQuery, (user_email,groupName))
+    groupData = cursor.fetchone()
+    if (groupData):
+        cursor.rownumber=0
+        error = "You have already created a group with this name"
+        displayNewGroup = "true"
+        return render_template('newGroup.html',displayNewGroup=displayNewGroup, error=error)
+    else:
+        newGroupQuery = 'INSERT INTO FriendGroup (owner_email,fg_name,description) VALUES (%s,%s,%s)'
+        cursor.execute(newGroupQuery,(user_email,groupName,groupDesc))
+        cursor.rownumber=0
+        if request.form.get('AddMember') == 'AddMember':
+            displayAddMember = "true"
+            return render_template('newGroup.html',displayAddMember=displayAddMember, dispGroupName=groupName)
+
+        # YOU NEED TO FIX THIS TO ACTUALLY CREATE A GROUP
+    return render_template('newGroup.html', displayNewGroup = 'true') #fix so it goes back to home.
+   
+    # newMemberQuery = 'INSERT INTO Belong (email,owner_email,fg_name) VALUES (%s,%s,%s)'
+    # cursor.execute(newMemberQuery,(newMember,user_email,groupName))
+    # conn.commit();
+    # cursor.close();
+
+    # if request.form['submit_button'] == addNewMember:
+
+@app.route('/addNewMember', methods=['GET','POST'])
+def addNewMember():
+    user_email = session['userEmail']
+    groupName = request.form['groupName']
+    newMember = request.form['newMember']
+    #check that the member you're adding exists
+    cursor = conn.cursor()
+    checkExist = 'SELECT * FROM Person WHERE email =(%s)'
+    cursor.execute(checkExist,newMember)
+    memExist = cursor.fetchone()
+    if (memExist):
+        # if the member exists - check if they're already in your group
+        checkMemQuery = 'SELECT email FROM Belong WHERE owner_email =(%s) AND fg_name = (%s)'
+        cursor.execute(checkMemQuery, (user_email,groupName))
+        memExistData = cursor.fetchone()
+        #if they're already in your group send an error message
+        if (memExistData):
+            cursor.rownumber =0
+            displayAddMember = "true"
+            error = "This person is already in your group"
+            return render_template('newGroup.html', displayAddMember = displayAddMember, dispGroupName = groupName, error=error)
+        else:
+            #member exists and is not in group so add the to your group
+            cursor.rownumber = 0
+            addMemberQuery = 'INSERT INTO Belong (email, owner_email, fg_name) VALUES (%s,%s,%s)'
+            cursor.execute(addMemberQuery, (newMember,user_email,groupName))
+            displayAddMember = "true"
+            message = "You successfully added a member"
+            return render_template('newGroup.html',displayAddMember=displayAddMember,dispGroupName=groupName, message = message)
+    #member doesn't exist
+    else:
+        cursor.rownumber=0
+        displayAddMember = "true"
+        error = "This person does not exist, try another email"
+        return render_template('newGroup.html',displayAddMember=displayAddMember, dispGroupName = groupName, error=error)
+
+    
+    checkMemQuery = 'SELECT email FROM Belong WHERE owner_email =(%s) AND fg_name = (%s)'
+    cursor.execute(checkMemQuery, (user_email,groupName))
+    memExistData = cursor.fetchone()
+    if (memExistData):
+        cursor.rownumber =0
+        error = "This person is already in your group"
+
+
 
 if __name__ == "__main__":
     app.run('127.0.0.1', 5000, debug=True)
